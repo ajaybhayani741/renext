56b90e1c6f0300ba7f094a8de9b548dfa312b616
Jeniksha Tagline <jbb@taglineinfotech.com>
2025-11-13 11:42:58 +0530

    RH-52 [disable password change and set option from edit user]

--- src/components/profile/container/profile.js
+++ src/components/profile/container/profile.js
@@ -1,11 +1,29 @@
 import { useState } from 'react'
 
+import { notifyMethod } from '../../../App'
+import useTranslations from '../../../hooks/useTranslations'
+import { userWiseRole } from '../../../utils/constant'
+import { isEqual } from '../../../utils/javascript'
+import { getItem } from '../../../utils/localstorage'
 import { tabKeys } from '../profile.description'
 
 const profile = () => {
+  const { t } = useTranslations()
+  const userData = JSON.parse(getItem('userData'))
+  const { roleId } = { ...userData }
+  const { inspectionOfficer } = userWiseRole
   const [currentTab, setCurrentTab] = useState(tabKeys.basicInfo)
 
   const handleTabChange = value => {
+    if (
+      isEqual(roleId, inspectionOfficer) &&
+      isEqual(value, tabKeys.password)
+    ) {
+      notifyMethod.warning({
+        message: t('msg_ForgetPasswordConsultDistrictCollector'),
+      })
+      return
+    }
     setCurrentTab(value)
   }
 
