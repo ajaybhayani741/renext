63f2d169ce4d29965ffb01155994ed8dd7a81f51
Jeniksha Tagline <jbb@taglineinfotech.com>
2026-04-10 18:01:37 +0530

    RH-114 [Revert button for completed jobs]

--- src/components/notifications/presentation/index.jsx
+++ src/components/notifications/presentation/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 import useTranslations from '../../../hooks/useTranslations'
 import ANTDSpin from '../../../shared/antd/ANTDSpin'
-import { length } from '../../../utils/javascript'
+import { isEqual, length } from '../../../utils/javascript'
 import notifications from '../container/notifications.container'
 import '../notification.scss'
 
@@ -23,24 +23,39 @@ const Notifications = () => {
       <div className="notification-container mt-15" ref={infiniteRef}>
         <List>
           {notificationsList && length(notificationsList)
-            ? notificationsList?.map(notification => (
-                <div
-                  key={notification?.id}
-                  className={`item-container ${!notification?.read ? 'new' : ''}`}
-                  onClick={() => handleNotificationClick(notification)}
-                >
-                  <List.Item key={notification?.id}>
-                    <div className={`item-wrapper type-job`}>
-                      <p>
-                        {t('msg_hostelAssignedForInspection', {
-                          hostelName:
-                            notification?.notificationContentDto?.hostelName,
-                        })}
-                      </p>
-                    </div>
-                  </List.Item>
-                </div>
-              ))
+            ? notificationsList?.map(notification => {
+                const isRevertJob = isEqual(notification?.type, 'REVERT_JOB')
+                return (
+                  <div
+                    key={notification?.id}
+                    className={`item-container ${!notification?.read ? 'new' : ''} ${
+                      isRevertJob ? 'revert-job' : ''
+                    }`}
+                    onClick={
+                      isRevertJob
+                        ? undefined
+                        : () => handleNotificationClick(notification)
+                    }
+                  >
+                    <List.Item key={notification?.id}>
+                      <div className={`item-wrapper type-job`}>
+                        <p>
+                          {t(
+                            isRevertJob
+                              ? 'msg_JobRevertedForInspection'
+                              : 'msg_hostelAssignedForInspection',
+                            {
+                              hostelName:
+                                notification?.notificationContentDto
+                                  ?.hostelName,
+                            },
+                          )}
+                        </p>
+                      </div>
+                    </List.Item>
+                  </div>
+                )
+              })
             : !loading && <List.Item>{t('noData')}</List.Item>}
         </List>
         <div className="text-center"> {isLoadingMore && <ANTDSpin />}</div>
