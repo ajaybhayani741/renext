acd168a5fd38a49ad42694d61b12f5e7c0c985f6
Jeniksha Tagline <jbb@taglineinfotech.com>
2026-07-03 12:41:30 +0530

    RH-127 [resolve upload profile pic]

--- src/components/userManagement/user.description.js
+++ src/components/userManagement/user.description.js
@@ -173,7 +173,7 @@ const addUserForm = {
     acceptFileTypes: '.png,.jpg,.jpeg,.webp',
     hasApiCall: false,
     filePath: ['profile'],
-    disableGalleryUpload: true,
+    disableGalleryUpload: false,
     md: 12,
     xs: 12,
   },
