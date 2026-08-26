85e7735457ed43ca66db44aff9683994f38d79db
Jeniksha Tagline <jbb@taglineinfotech.com>
2026-08-26 14:14:05 +0530

    RH-144 [set username and password for add/update user]

--- src/components/userManagement/container/addUser.container.js
+++ src/components/userManagement/container/addUser.container.js
@@ -88,6 +88,7 @@ const addUser = ({
     (include(childUsers, formRoleId) ? childUserFormFields : userFormFields)
   const normalizedFormField = { ...formField }
   if (
+    editInfo?.data?.id &&
     !isEqual(roleId, districtCollector) &&
     include([inspectionOfficer, mandalSpecialOfficer], formRoleId)
   ) {
@@ -281,10 +282,11 @@ const addUser = ({
                   },
                 }),
               ...editForm,
-              ...(isCredentialUser && {
-                username: { ...prevForm.username, disabled: true },
-                password: { ...prevForm.password, required: false },
-              }),
+              ...(isCredentialUser &&
+                isEqual(roleId, districtCollector) && {
+                  username: { ...prevForm.username, disabled: true },
+                  password: { ...prevForm.password, required: false },
+                }),
             }
       })
     }
