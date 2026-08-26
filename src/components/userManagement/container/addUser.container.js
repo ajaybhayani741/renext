b347a0c6b1de74f3181babe082170b4d4a28bcdd
Jeniksha Tagline <jbb@taglineinfotech.com>
2026-08-26 17:21:44 +0530

    RH-144 [Resolve edit username, password, resolve mandal list in update user]

--- src/components/userManagement/container/addUser.container.js
+++ src/components/userManagement/container/addUser.container.js
@@ -101,8 +101,10 @@ const addUser = ({
   const normalizedFormField = { ...formField }
   if (
     editInfo?.data?.id &&
-    !isEqual(roleId, districtCollector) &&
-    include([inspectionOfficer, mandalSpecialOfficer], formRoleId)
+    ((!isEqual(roleId, districtCollector) &&
+      include([inspectionOfficer, mandalSpecialOfficer], formRoleId)) ||
+      (isEqual(roleId, districtCollector) &&
+        isEqual(districtCollector, formRoleId)))
   ) {
     delete normalizedFormField.username
     delete normalizedFormField.password
@@ -296,7 +298,9 @@ const addUser = ({
                     },
                   }
                 : {}),
-              ...userFormByRoleId(t, form.getFieldValue())?.[formRoleId],
+              ...userFormByRoleId(t, form.getFieldValue(), mandalList)?.[
+                formRoleId
+              ],
               ...(isCredentialUser && {
                 username: {
                   ...prevForm.username,
@@ -325,10 +329,11 @@ const addUser = ({
                   },
                 }),
               ...editForm,
-              ...(isCredentialUser && isEqual(roleId, districtCollector) && {
-                username: { ...prevForm.username, disabled: true },
-                password: { ...prevForm.password, required: false },
-              }),
+              ...(isCredentialUser &&
+                isEqual(roleId, districtCollector) && {
+                  username: { ...prevForm.username, disabled: true },
+                  password: { ...prevForm.password, required: false },
+                }),
             }
       })
     }
