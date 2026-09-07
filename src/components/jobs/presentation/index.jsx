983df7115728c92a60ac8965ade3921319d06f5e
Jeniksha Tagline <jbb@taglineinfotech.com>
2026-09-07 17:46:30 +0530

    RH-149 [deselect date filter button]

--- src/components/jobs/presentation/index.jsx
+++ src/components/jobs/presentation/index.jsx
@@ -170,7 +170,11 @@ const JobManagement = ({
             <>
               <div className="d-flex flex-end">
                 <FiscalYearSelect
-                  onDateChange={(from, to) => apiCall(1, { from, to })}
+                  onDateChange={
+                    isEqual(jobType, tabKeys.inspection)
+                      ? undefined
+                      : (from, to) => apiCall(1, { from, to })
+                  }
                   showRecentPresets
                   showDateShortcutButtons={
                     isEqual(jobType, tabKeys.inspection) &&
